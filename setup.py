efe9b7c7ea4808cc5b7c237aa626c6a2a442af60
marferdiaz <marferdiaz@gmail.com>
2020-10-01 15:49:13 +0200

    Removed dependencies not used

--- setup.py
+++ setup.py
@@ -10,11 +10,7 @@ setup(
     packages=find_packages('.'),
     python_requires='>=3.6',
     install_requires=[
-        'transitions==0.6.9',
-        'pygraphviz==1.5',
-        'dill',
         'scikit-learn',
-        'matplotlib',
         'numpy==1.16.4', 
         'Keras==2.2.4',
         'tensorflow==1.14.0',
